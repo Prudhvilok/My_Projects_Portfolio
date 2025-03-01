9d5346b32827feaeb9259bf312cd4bdfdb5b0be6
Prudhvilok <prudhvilokattur@gmail.com>
2025-03-01 16:27:18 -0600

    Delete assets/js/sidebar.js

--- assets/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".dropdown-toggle").forEach(function(btn) {
-        btn.addEventListener("click", function() {
-            let dropdownMenu = this.nextElementSibling;
-            dropdownMenu.classList.toggle("active");
-        });
-    });
-});
